e78b645f363e050357298804ed2c33ebdfdee195
jamesmhee <nontasabs@gmail.com>
2023-07-05 02:10:19 +0700

    edit home and about and edit to one page website

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import arprofile from '../assets/arprofile.png'
 
 const About = () => {
   return (
-    <div id="about" className="bg-about-2 bg-slate-900 bg-cover bg-center flex md:flex-row flex-col justify-center items-center text-slate-100 min-h-screen">
+    <div id="about" className="bg-about-2 bg-cover md:bg-cover md:bg-center bg-right flex md:flex-row flex-col justify-center items-center text-slate-100 min-h-screen">
       <div className='md:order-1 order-2 md:w-7/12 grow-0 p-10 flex flex-col text-center md:text-right'>
         Hi I'm James. I was born on the 9th of August, 1999.<br></br>I'm graduated Information Technology major from King Mongkut's Institute of Technology Ladkrabang.<br></br>
         I'm Web Developer, I prefer to design and develop the website or mobile-app almost frontend.<br></br>I am extremely motivated to constantly develop my skills and grow professionally.

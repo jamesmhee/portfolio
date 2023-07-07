09a786ed8d6723eabf310df4c2db0b69f725db70
jamesmhee <nontasabs@gmail.com>
2023-07-08 06:19:40 +0700

    add emailjs to contact page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,22 +30,22 @@ const Navbar = () => {
   <>
     <nav id="navbar" className={`z-50 transition-all duration-700 ease-in-out m-3 p-5 ${scrollDown ? 'md:p-8 md:shadow-zero md:bg-zinc-900' : 'md:p-12 md:border-hidden md:bg-[#212121] '} md:m-0 bg-zinc-900 border-none border-2 sm:border-4 md:border-t-0 md:border-b-4 md:border-x-0 md:border-b-2 p-4 rounded-full md:rounded-none bg-transparent justify-center md:justify-around flex fixed inset-x-0 bottom-2 md:sticky md:top-0`}>
         <Link to="" duration={400} smooth={true} onClick={scrollToTop}>
-          <div className={`${scrollDown? 'md:text-lg' : 'md:text-xl'} transition-all duration-700 ease-in-out hidden md:block font-semibold text-slate-100 hover:text-rose-600 transition-all ease-in-out duration-300`}>
-            <p className='cursor-pointer'>jemmyblair</p>
+          <div className={`${scrollDown? 'md:text-lg' : 'md:text-xl'} transition-all duration-700 ease-in-out hidden md:block font-semibold text-rose-600 hover:text-white transition-all ease-in-out duration-300`}>
+            <p className='cursor-pointer transition-all duration-300 hover:scale-125'>jemmyblair</p>
           </div>        
         </Link>
-        <ul className={`${scrollDown? 'md:text-md' : 'md:text-md'} transition-all duration-700 ease-in-out flex gap-10 md:gap-24 font-semibold items-center text-sm sm:text-md`}>
+        <ul className={`${scrollDown? 'md:text-md' : 'md:text-md'} transition-all duration-500 ease-in-out flex gap-10 md:gap-24 font-semibold items-center text-sm sm:text-md`}>
           <Link to="" duration={400} smooth={true} onClick={scrollToTop}>
-            <li className='cursor-pointer md:hidden text-rose-600 hover:text-white transition-all ease-in-out'><ImHome size={30}/></li>                      
+            <li className='cursor-pointer text-rose-600 hover:text-white md:hidden transition-all ease-in-out'><ImHome size={30}/></li>                      
           </Link >
           <Link to="about" duration={400} smooth={true}>
-            <li className='cursor-pointer text-rose-600 hover:text-white transition-all ease-in-out'>ABOUT</li>          
+            <li className='cursor-pointer text-white hover:text-rose-600 transition-all ease-in-out hover:underline hover:decoration-white hover:decoration-4 hover:underline-offset-8'>ABOUT</li>          
           </Link>
           <Link to="project" duration={400} smooth={true}>
-            <li className='cursor-pointer text-rose-600 hover:text-white transition-all ease-in-out'>PROJECT</li>          
+            <li className='cursor-pointer text-white hover:text-rose-600 transition-all ease-in-out hover:underline hover:decoration-white hover:decoration-4 hover:underline-offset-8'>PROJECT</li>          
           </Link>
           <Link to="contact" duration={400} smooth={true}>
-            <li className='cursor-pointer text-rose-600 hover:text-white transition-all ease-in-out'>CONTACT</li>          
+            <li className='cursor-pointer text-white hover:text-rose-600 transition-all ease-in-out hover:underline hover:decoration-white hover:decoration-4 hover:underline-offset-8'>CONTACT</li>          
           </Link>
         </ul>
     </nav>
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -3,7 +3,7 @@ import Slider from '../components/Slider'
 
 const Project = () => {
   return (
-    <div  id="project"  className='flex md:flex-row flex-col md:p-20 p-10 justify-center gap-10 items-center min-h-screen'>
+    <div  id="project"  className='flex md:flex-row flex-col my-10 md:p-20 p-10 justify-center gap-10 items-center h-full'>
       <div  className='font-semibold text-lg text-rose-600 text-center uppercase underline decoration-wavy decoration-zinc-900 underline-offset-8 w-auto md:w-4/12'>
         my project
       </div>
